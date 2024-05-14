2f862b57aff132c68495a55d72ab05495846fca9
ChrisHo0317 <chris19980317@gmail.com>
2024-05-15 01:39:03 +0800

    20240514 backtest V1

--- main_binance.py
+++ main_binance.py
@@ -5,15 +5,14 @@ from binance.um_futures import UMFutures
 import pandas as pd
 import json
 
-def message_handler(_, message):
-    print(pd.DataFrame(json.loads(message),index=[0]))
+
 columns = ['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote Asset Volume','Number of Trades', 'TB Base Volume', 'TB Quote Volume', 'Ignore']
-Currency = "BTCUSDT".upper() # KASUSDT LEVERUSDT PEOPLEUSDT 
-Kline_count = 1000000
+Currency = "LEVERUSDT".upper() # KASUSDT LEVERUSDT PEOPLEUSDT 
+Kline_count = 1000
 Mode = "USDT"
 if "USDT"==Mode:
     um_futures_client = UMFutures()
-    data_perp_15m = um_futures_client.mark_price_klines(Currency, "1m", **{"limit": Kline_count})
+    data_perp_15m = um_futures_client.mark_price_klines(Currency, "15m", **{"limit": Kline_count})
     data_perp_1h = um_futures_client.mark_price_klines(Currency, "1h", **{"limit": round(Kline_count/4)})
 
 elif 'USDC'==Mode:
